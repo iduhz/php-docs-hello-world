c56b4d85df9f810753cf95f6c66f1089ed072c22
iduhz <iduhz@users.noreply.github.com>
2023-09-03 00:58:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,35 @@
 <?php
 
-$oauthRequest = generateRequest('grant_type=authorization_code&client_id=' . _OAUTH_CLIENTID . '&redirect_uri=' . urlencode(_URL . '/oauth.php') . '&code=' . $_GET['code'] . '&code_verifier=' . $sessionData['txtCodeVerifier']);
+$oAuth = new modOAuth();
 
-$response = postRequest('token', $oauthRequest);
+$oauthRequest = $oAuth->generateRequest('grant_type=authorization_code&client_id=' . 'f2924b54-9525-4521-ae9f-a85652060e6f' . '&redirect_uri=' . urlencode('http://localhost:3000'. '/oauth.php') . '&code=' . $_GET['code'] . '&code_verifier=' . $sessionData['txtCodeVerifier']);
+
+$response = $oAuth->postRequest('token', $oauthRequest);
 
 	$reply = json_decode($response);
 
 
 echo reply;
 
-function generateRequest($data) {
-	return $data . '&client_secret=' . urlencode(_OAUTH_SECRET);
-}
+class modOAuth {
 
-function postRequest($endpoint, $data) {
-	$ch = curl_init('https://login.microsoftonline.com/2685ce0d-979a-47d6-839d-a4b8a792138c/oauth2/v2.0/' . $endpoint);
-	curl_setopt($ch, CURLOPT_POST, 1);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-	$response = curl_exec($ch);
-	if ($cError = curl_error($ch)) {
-		echo $this->errorMessage($cError);
-		exit;
+	function generateRequest($data) {
+		return $data;
 	}
-	curl_close($ch);
-	return $response;
 
+	function postRequest($endpoint, $data) {
+		$ch = curl_init('https://login.microsoftonline.com/2685ce0d-979a-47d6-839d-a4b8a792138c/oauth2/v2.0/' . $endpoint);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$response = curl_exec($ch);
+		if ($cError = curl_error($ch)) {
+			echo $cError;
+			exit;
+		}
+		curl_close($ch);
+		return $response;
+
+	}
 }

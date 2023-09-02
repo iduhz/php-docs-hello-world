096c799b52fb1d6caed751c60e2676cf1e951656
iduhz <iduhz@users.noreply.github.com>
2023-09-03 01:05:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,21 @@
 
 $oAuth = new modOAuth();
 
-$oauthRequest = $oAuth->generateRequest('grant_type=authorization_code&client_id=' . 'f2924b54-9525-4521-ae9f-a85652060e6f' . '&redirect_uri=' . urlencode('http://localhost:3000'. '/oauth.php') . '&code=' . $_GET['code'] . '&code_verifier=' . $sessionData['txtCodeVerifier']);
+$data = 'grant_type=authorization_code&client_id=' . 'f2924b54-9525-4521-ae9f-a85652060e6f' . '&redirect_uri=' . urlencode('https://easysignatureportal.azurewebsites.net'. '/oauth.php') . '&code=' . $_GET['code'] . '&code_verifier=' . $sessionData['txtCodeVerifier']
 
-$response = $oAuth->postRequest('token', $oauthRequest);
+$ch = curl_init('https://login.microsoftonline.com/2685ce0d-979a-47d6-839d-a4b8a792138c/oauth2/v2.0/token');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-	$reply = json_decode($response);
+$response = curl_exec($ch);
+if ($cError = curl_error($ch)) {
+	echo $cError;
+	exit;
+}
+curl_close($ch);
+		
+$reply = json_decode($response);
 
 
 echo reply;
-
-class modOAuth {
-
-	function generateRequest($data) {
-		return $data;
-	}
-
-	function postRequest($endpoint, $data) {
-		$ch = curl_init('https://login.microsoftonline.com/2685ce0d-979a-47d6-839d-a4b8a792138c/oauth2/v2.0/' . $endpoint);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($ch);
-		if ($cError = curl_error($ch)) {
-			echo $cError;
-			exit;
-		}
-		curl_close($ch);
-		return $response;
-
-	}
-}

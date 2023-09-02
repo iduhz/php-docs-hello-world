8f8468d2b58e615abd5012f0ca54f8122218ba7a
iduhz <iduhz@users.noreply.github.com>
2023-09-03 00:51:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,46 +10,21 @@ $response = postRequest('token', $oauthRequest);
 echo reply;
 
 function generateRequest($data) {
-	        if (_OAUTH_METHOD == 'certificate') {
-                        // Use the certificate specified
-                        //https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-certificate-credentials
-                        $cert = file_get_contents(_OAUTH_AUTH_CERTFILE);
-                        $certKey = openssl_pkey_get_private(file_get_contents(_OAUTH_AUTH_KEYFILE));
-                        $certHash = openssl_x509_fingerprint($cert);
-                        $certHash = base64_encode(hex2bin($certHash));
-                        $caHeader = json_encode(array('alg' => 'RS256', 'typ' => 'JWT', 'x5t' => $certHash));
-                        $caPayload = json_encode(array('aud' => 'https://login.microsoftonline.com/' . _OAUTH_TENANTID . '/v2.0',
-                                                'exp' => date('U', strtotime('+10 minute')),
-                                                'iss' => _OAUTH_CLIENTID,
-                                                'jti' => $this->uuid(),
-                                                'nbf' => date('U'),
-                                                'sub' => _OAUTH_CLIENTID));
-                        $caSignature = '';
+	return $data . '&client_secret=' . urlencode(_OAUTH_SECRET);
+}
 
-                        $caData = $this->base64UrlEncode($caHeader) . '.' . $this->base64UrlEncode($caPayload);
-                        openssl_sign($caData, $caSignature, $certKey, OPENSSL_ALGO_SHA256);
-                        $caSignature = $this->base64UrlEncode($caSignature);
-                        $clientAssertion = $caData . '.' . $caSignature;
-                        return $data . '&client_assertion=' . $clientAssertion . '&client_assertion_type=urn:ietf:params:oauth:client-assertion-type:jwt-bearer';
-                } else {
-			// Use the client secret instead
-                        return $data . '&client_secret=' . urlencode(_OAUTH_SECRET);
-                }
+function postRequest($endpoint, $data) {
+	$ch = curl_init('https://login.microsoftonline.com/2685ce0d-979a-47d6-839d-a4b8a792138c/oauth2/v2.0/' . $endpoint);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+	$response = curl_exec($ch);
+	if ($cError = curl_error($ch)) {
+		echo $this->errorMessage($cError);
+		exit;
 	}
+	curl_close($ch);
+	return $response;
 
-	function postRequest($endpoint, $data) {
-		$ch = curl_init('https://login.microsoftonline.com/' . _OAUTH_TENANTID . '/oauth2/v2.0/' . $endpoint);
-		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$response = curl_exec($ch);
-		if ($cError = curl_error($ch)) {
-			echo $this->errorMessage($cError);
-			exit;
-		}
-		curl_close($ch);
-		return $response;
-
-	}
+}

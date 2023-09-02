d472d1dfc9bad4b9d9be75258b317d1bdb7d0e79
iduhz <iduhz@users.noreply.github.com>
2023-09-03 00:08:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
 
 echo "Hello World!";
+
+$url = 'https://easysignaturefunctionapp.azurewebsites.net/api/HelloWorld2';
+
+
+
+$ch = curl_init($url);                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                        
+
+$server_output = curl_exec ($ch);
+
+echo  $server_output;
